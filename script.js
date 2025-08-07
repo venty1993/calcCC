243bbfae43e6009bd048dcb1d54ae311e90fb254
venty1993 <75829033+venty1993@users.noreply.github.com>
2025-08-07 22:05:47 +0900

    submit

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+const ccSize = {
+    big : 150,
+    small : 70
+}
+
+const ccType = {
+    hala : {
+        크림치즈 : 560/655,
+        베이컨 : 43/655,
+        할라피뇨 : 52/655
+    },
+    truffle : {
+        크림치즈 : 700/715,
+        후추 : 1/715,
+        오일 : 14/715
+    },
+    salmon : {
+        연어CC : 493/721.9,
+        소금 :1.4/721.9,
+        후추 :2.5/721.9,
+        연어 :100/721.9,
+        양파 :100/721.9,
+        케이퍼 :25/721.9
+    },
+    fig : {
+        연어CC : 2040/2423,
+        무화과 :204 / 2423,
+        호두 : 128 /2423,
+        설탕 : 51 / 2423
+    }
+}
+
+const allInputs = document.getElementsByTagName('input')
+
+console.log(allInputs)
+for (const element of allInputs) {
+    element.addEventListener('change', ()=>{
+        calcCC(element.classList[1])
+    })
+    
+}
+
+function calcCC(type) {
+    const inputs = document.getElementsByClassName(type)
+    const targetConatiner = document.querySelector(`.cc-calculate-result.${type}`)
+    console.log(targetConatiner)
+    
+    let total = 0;
+
+    total += parseInt(inputs.item(0).value)*150
+    total += parseInt(inputs.item(1).value)*70
+
+    if(isNaN(total)){
+        console.log('test?')
+        targetConatiner.innerHTML='숫자를 입력하쇼'
+        return;
+    }
+
+    makeRow();
+
+    function makeRow(){
+        targetConatiner.innerHTML =`total : ${total}`
+
+        const select = ccType[type];
+        
+        for (const key in select) {
+            
+                const element = select[key];
+                
+                const rowElement = document.createElement('div');
+                rowElement.classList.add('cc-result-row')
+                
+                const nameElement = document.createElement('p')
+                nameElement.innerText = `${key} : `;
+
+
+                const weightElement = document.createElement('p')
+                weightElement.innerText = (total * element).toFixed(2);
+
+                rowElement.appendChild(nameElement)
+                rowElement.appendChild(weightElement)
+
+                targetConatiner.appendChild(rowElement)
+            }
+        }
+    
+}
